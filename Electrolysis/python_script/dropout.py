40a13b7532f1c71cc6e0f5463cb530e04d5aff24
donRumata <donrumata03@gmail.com>
2020-10-16 21:22:08 +0300

    add shifting and dS

--- Electrolysis/python_script/dropout.py
+++ Electrolysis/python_script/dropout.py
@@ -4,6 +4,8 @@ from smothing_tester import *
 
 from typing import *
 
+def line_value(line : Tuple[float, float], _x : float):
+	return _x * line[0] + line[1]
 
 def line_point_distance(point : Tuple[float, float], line : Tuple[float, float], add_sign : bool):
 	line_A = line[0]
@@ -13,6 +15,8 @@ def line_point_distance(point : Tuple[float, float], line : Tuple[float, float],
 	numerator = line_A * point[0] + line_B * point[1] + line_C
 	if not add_sign:
 		numerator = abs(numerator)
+	else:
+		numerator *= -1
 	denominator = np.sqrt(line_A ** 2 + line_B ** 2)
 
 	return numerator / denominator
@@ -22,6 +26,7 @@ def line_point_distance(point : Tuple[float, float], line : Tuple[float, float],
 
 print("Line: k =", linear_coefficients[0], ", b =", linear_coefficients[1])
 distances_from_the_line = [line_point_distance(point, linear_coefficients, True) for point in current_time_dependency]
+unsigned_distances_from_the_line = [line_point_distance(point, linear_coefficients, False) for point in current_time_dependency]
 distance_distribution = count_density(distances_from_the_line, 0.1, 1000, smoothing_normal)
 
 distance_sigma = statistics.stdev(distances_from_the_line)
@@ -31,15 +36,15 @@ n_sigma = 1.5 * distance_sigma
 max_allowed_distance_from_line = n_sigma
 
 good_point_indexes = [i for i in range(len(current_time_dependency)) if line_point_distance(current_time_dependency[i], linear_coefficients, False) < max_allowed_distance_from_line]
-print(len(good_point_indexes), "/", len(current_time_dependency), good_point_indexes)
+print("Good points:", len(good_point_indexes), "/", len(current_time_dependency), good_point_indexes)
 
 
-#
-distance_distribution_max_val = max([i[1] for i in distance_distribution])
-plt.plot([i[0] for i in distance_distribution], [i[1] for i in distance_distribution])
-plt.plot([-n_sigma, -n_sigma], [0, distance_distribution_max_val], linestyle = "--", color = "red")
-plt.plot([+n_sigma, +n_sigma], [0, distance_distribution_max_val], linestyle = "--", color = "red")
-plt.show()
+if __name__ == '__main__':
+	distance_distribution_max_val = max([i[1] for i in distance_distribution])
+	plt.plot([i[0] for i in distance_distribution], [i[1] for i in distance_distribution])
+	plt.plot([-n_sigma, -n_sigma], [0, distance_distribution_max_val], linestyle = "--", color = "red")
+	plt.plot([+n_sigma, +n_sigma], [0, distance_distribution_max_val], linestyle = "--", color = "red")
+	plt.show()
 
 
 
--- Electrolysis/python_script/shifting.py
+++ Electrolysis/python_script/shifting.py
@@ -7,11 +7,53 @@ good_index_set = set(good_point_indexes)
 good_points = []
 bad_points = []
 
-for index, point in enumerate(curent_time_dependency):
+for index, point in enumerate(current_time_dependency):
 	if index in good_index_set:
 		good_points.append(point)
 	else:
 		bad_points.append(point)
+
+# print(good_points)
+# print(bad_points)
+
+smallest_good_point_with_distance = min([(line_point_distance(point, linear_coefficients, True), point) for index, point in enumerate(good_points)])
+biggest_good_point_with_distance = max([(line_point_distance(point, linear_coefficients, True), point) for index, point in enumerate(good_points)])
+
+smallest_good_point = smallest_good_point_with_distance[1]
+biggest_good_point = biggest_good_point_with_distance[1]
+
+upper_line = (linear_coefficients[0], linear_coefficients[1] - line_value(linear_coefficients, biggest_good_point[0]) + biggest_good_point[1])
+bottom_line = (linear_coefficients[0], linear_coefficients[1] - line_value(linear_coefficients, smallest_good_point[0]) + smallest_good_point[1])
+
+# print("Upper line is:", upper_line)
+
+lowest_area = count_graph_area(zip([i[0] for i in current_time_dependency], np.polyval(bottom_line, [i[0] for i in current_time_dependency])))
+highest_area = count_graph_area(zip([i[0] for i in current_time_dependency], np.polyval(upper_line, [i[0] for i in current_time_dependency])))
+print("Min possible area:", lowest_area)
+print("Max possible area:", highest_area)
+
+upper_dS = highest_area - count_graph_area(smoothed_current_time_dependency)
+lower_dS = count_graph_area(smoothed_current_time_dependency) - lowest_area
+average_dS = (upper_dS + lower_dS) / 2
+
+print("dx's:", lower_dS, upper_dS, average_dS)
+
+if __name__ == '__main__':
+	plt.ylim(0, max([i[1] for i in current_time_dependency]) * 1.1)
+	plt.xlabel("t, sec")
+	plt.ylabel("I, A")
+	plt.title("Current vs. Time dependency")
+	plt.plot([i[0] for i in current_time_dependency], linearly_approximated_current_time_dependency, label = "Linearly approximated graph")
+	plt.scatter([i[0] for i in good_points], [i[1] for i in good_points], color = "green", label = "Close points")
+	plt.scatter([i[0] for i in bad_points], [i[1] for i in bad_points], color = "red", label = "Faraway points")
+
+	plt.plot([i[0] for i in current_time_dependency], np.polyval(upper_line, [i[0] for i in current_time_dependency]),
+		color = "red", linestyle = "--", label = "Upper line")
+	plt.plot([i[0] for i in current_time_dependency], np.polyval(bottom_line, [i[0] for i in current_time_dependency]),
+		color = "red", linestyle = "--", label = "Lower line")
+
+	plt.legend()
+	plt.show()
 
 
 
