92b4232d1053746e5a7c075ec9b961df92b313b1
donRumata <donrumata03@gmail.com>
2020-11-10 19:56:42 +0300

    Create Astronomy project, make data processing

---
+++ AstronomyProject/measurements.py
@@ -0,0 +1,60 @@
+import math
+import numpy as np
+from typing import *
+from datetime import date, timedelta
+
+
+
+n_days = 48
+initial_date = date(2020, 9, 24)
+
+"""
+	Day index starts from zero for the first measurement day and is [0, n_days)
+	
+	In GoogleDocs(https://docs.google.com/spreadsheets/d/1F_DoQ4brBQePHNCiOVG615uoBIPiO2jDlzrxd8Wwrc4/):
+		date index is `row_number - 2`
+"""
+def day_index_to_date(day_index: int):
+	assert day_index < n_days
+	initial_date + timedelta(days=day_index)
+
+def date_to_day_index(this_date: date):
+	return (this_date - initial_date).days
+
+########
+
+def process_measurement_data(data: List[Tuple[int, float]]):
+	return [(day_index_to_date(measurement[0]), math.atan(measurement[1])) for measurement in data]
+
+# Data:         ( Day index | measured tangent ) --> converted to   ( Date | degree )
+
+
+real_data_by_vova = process_measurement_data([
+
+])
+
+real_data_by_andrew = process_measurement_data([
+
+])
+
+real_data_by_sergey = process_measurement_data([
+
+])
+
+real_data_by_nikita = process_measurement_data([
+
+])
+
+
+all_real_data = sorted(
+	real_data_by_vova[:] +
+	real_data_by_andrew[:] +
+	real_data_by_sergey[:] +
+	real_data_by_nikita[:]
+)
+
+
+# real_measurement_data = [
+#
+# ]
+
